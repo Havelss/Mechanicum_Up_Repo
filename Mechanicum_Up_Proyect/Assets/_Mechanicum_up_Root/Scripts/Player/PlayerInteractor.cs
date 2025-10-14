8f72689e5f6c6022b1319dd826bbcbac1e500649
Havelss <havels05@gmail.com>
2025-10-14 13:39:33 +0200

    Extendi el Fondo

---
+++ Mechanicum_Up_Proyect/Assets/_Mechanicum_up_Root/Scripts/Player/PlayerInteractor.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerInteractor : MonoBehaviour
+{
+    [Header("Respawn Configuration")]
+    [SerializeField] Transform respawnPoint; //Posicion del respawn
+    [SerializeField] float respawnFallLimit; // Limite en -y que de ser alcanzado, respawnea
+    Rigidbody playerRB;
+
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (transform.position.y <= respawnFallLimit) Respawn();  
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        playerRB.linearVelocity = new Vector3(0,0,0);  //reinicia la velocidad del player
+        transform.position = respawnPoint.position;
+    }
+}
