040fa48417364e2556aea97dd5a234970c40af2f
Sadh0w <alexgalindosandin@gmail.com>
2025-10-15 13:06:57 +0200

    a

--- Mechanicum_Up_Proyect/Assets/_Mechanicum_up_Root/Scripts/Interactables/Interactor.cs
+++ Mechanicum_Up_Proyect/Assets/_Mechanicum_up_Root/Scripts/Interactables/Interactor.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 
 public class Interactor : MonoBehaviour
+
+/*
 {
     [SerializeField] private Transform interactionPoint;
     [SerializeField] private float interactionPointRadius = 0.5f;
@@ -39,6 +41,58 @@ public class Interactor : MonoBehaviour
             if(_interactionPrompUI.IsDisplayed) _interactionPrompUI.Close();
         }
 
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(interactionPoint.position, interactionPointRadius);
+    }
+}
+*/
+{
+    [SerializeField] private Transform interactionPoint;
+    [SerializeField] private float interactionPointRadius = 0.5f;
+    [SerializeField] private LayerMask interactable;
+    [SerializeField] private InteractionPromptUI _interactionPrompUI;
+
+    private readonly Collider[] colliders = new Collider[3];
+    [SerializeField] private int numFound;
+
+    private IInteractable _interactable;
+
+    void Update()
+    {
+        numFound = Physics.OverlapSphereNonAlloc(interactionPoint.position, interactionPointRadius, colliders, interactable);
+
+        if (numFound > 0)
+        {
+            _interactable = colliders[0].GetComponent<IInteractable>();
+
+            if (_interactable != null)
+            {
+                if (!_interactionPrompUI.IsDisplayed)
+                    _interactionPrompUI.SetUp(_interactable.InteractionPrompt);
+
+                if (Keyboard.current.eKey.wasPressedThisFrame)
+                    _interactable.Interact(this);
+            }
+        }
+        else
+        {
+            if (_interactable != null)
+            {
+                if (_interactable is SO_Terminal terminal)
+                {
+                    terminal.CloseTerminal();
+                }
+
+                _interactable = null;
+            }
+
+            if (_interactionPrompUI.IsDisplayed)
+                _interactionPrompUI.Close();
+        }
     }
 
     private void OnDrawGizmos()
