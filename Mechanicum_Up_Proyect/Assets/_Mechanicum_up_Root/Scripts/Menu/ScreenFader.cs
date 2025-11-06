32575a6b3850d316c3a3eee9d19dacba750ab3fb
Sadh0w <alexgalindosandin@gmail.com>
2025-11-06 13:36:49 +0100

    transicion entre niveles

---
+++ Mechanicum_Up_Proyect/Assets/_Mechanicum_up_Root/Scripts/Menu/ScreenFader.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScreenFader : MonoBehaviour
+{
+    public static ScreenFader Instance { get; private set; }
+
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeDuration = 1.5f;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public IEnumerator FadeOut()
+    {
+        fadeImage.gameObject.SetActive(true);
+        Color c = fadeImage.color;
+        c.a = 0f;
+        fadeImage.color = c;
+
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            c.a = Mathf.Clamp01(elapsed / fadeDuration);
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        c.a = 1f;
+        fadeImage.color = c;
+    }
+
+    public IEnumerator FadeIn()
+    {
+        fadeImage.gameObject.SetActive(true);
+        Color c = fadeImage.color;
+        c.a = 1f;
+        fadeImage.color = c;
+
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            c.a = 1f - Mathf.Clamp01(elapsed / fadeDuration);
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        fadeImage.gameObject.SetActive(false);
+    }
+}
