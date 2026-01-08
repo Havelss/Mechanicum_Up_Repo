da45c5d66e725ce2e318891847f339ada5981eca
Sadh0w <alexgalindosandin@gmail.com>
2026-01-08 08:59:00 +0100

    guardado por si acaso
    este si el de abajo no

--- Mechanicum_Up_Proyect/Assets/_Mechanicum_up_Root/Scripts/Interactables/Electricidad/PowerUpPickup.cs
+++ Mechanicum_Up_Proyect/Assets/_Mechanicum_up_Root/Scripts/Interactables/Electricidad/PowerUpPickup.cs
@@ -4,12 +4,30 @@ public class PowerUpPickup : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        var electricity = other.GetComponent<P_ElectricityArea>();
-        if (electricity != null)
+        Debug.Log($"El objeto {other.name} ha tocado el power-up.");
+
+        var powerUp = other.GetComponent<P_ElectricityPowerUp>();
+        if (powerUp != null)
         {
-            electricity.ActivatePower();
+            powerUp.ActivatePower();
             Debug.Log("⚡ Power-up de electricidad obtenido!");
             Destroy(gameObject);
         }
     }
 }
+
+//using UnityEngine;
+
+//public class PowerUpPickup : MonoBehaviour
+//{
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        var electricity = other.GetComponent<P_ElectricityArea>();
+//        if (electricity != null)
+//        {
+//            electricity.ActivatePower();
+//            Debug.Log("⚡ Power-up de electricidad obtenido!");
+//            Destroy(gameObject);
+//        }
+//    }
+//}
