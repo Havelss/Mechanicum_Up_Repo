693281cc7f41032abe4163ef3fb7f152199bb998
Sadh0w <alexgalindosandin@gmail.com>
2026-01-16 13:07:53 +0100

    caos resuelto

--- Mechanicum_Up_Proyect/Assets/_Mechanicum_up_Root/Scripts/vagoneta/MinecartController.cs
+++ Mechanicum_Up_Proyect/Assets/_Mechanicum_up_Root/Scripts/vagoneta/MinecartController.cs
@@ -1,128 +1,4 @@
-﻿//using UnityEngine;
-
-//[System.Serializable]
-//public class CartSpeedSettings
-//{
-//    public float lateralSpeed = 8f;
-//}
-
-//public class MinecartController : MonoBehaviour
-//{
-//    [Header("Referencias")]
-//    public Transform playerSeat;
-//    public MinecartTerminalUI cartTerminalUI;
-//    public MinecartTerminalAnimator terminalAnimator;
-//    public Rigidbody rb;
-
-//    [Header("Movimiento")]
-//    public float lateralSpeed = 8f;
-//    public float lateralAcceleration = 5f;
-
-//    [Header("Player")]
-//    public bool isPlayerInside = false;
-//    private Transform player;
-//    private PlayerController playerController;
-
-//    private float lateralDirection = 0f;
-//    private float currentLateralSpeed = 0f;
-
-//    private void Awake()
-//    {
-//        if (rb == null) rb = GetComponent<Rigidbody>();
-//        rb.useGravity = true;
-//        rb.constraints = RigidbodyConstraints.FreezeRotation;
-//    }
-
-//    private void Update()
-//    {
-//        if (isPlayerInside)
-//        {
-//            MoveCartLateral();
-//        }
-//    }
-
-//    private void FixedUpdate()
-//    {
-//        // Solo mover vagoneta
-//        MoveCartLateral();
-//    }
-
-//    private void MoveCartLateral()
-//    {
-//        float targetSpeed = lateralDirection * lateralSpeed;
-//        currentLateralSpeed = Mathf.MoveTowards(currentLateralSpeed, targetSpeed, lateralAcceleration * Time.fixedDeltaTime);
-
-//        // Movimiento con rb.velocity
-//        Vector3 vel = rb.linearVelocity;
-//        vel.x = currentLateralSpeed;
-//        rb.linearVelocity = vel;
-//    }
-
-//    #region Métodos de movimiento lateral
-//    public void MoveLeft() => lateralDirection = -1f;
-//    public void MoveRight() => lateralDirection = 1f;
-//    public void StopLateral() => lateralDirection = 0f;
-//    #endregion
-
-//    #region Player Enter/Exit
-//    public void FinalizeEnter(Transform playerObj)
-//    {
-//        player = playerObj;
-//        playerController = player.GetComponent<PlayerController>();
-
-//        if (playerController != null) playerController.enabled = false;
-
-//        Rigidbody prb = player.GetComponent<Rigidbody>();
-//        Collider pcol = player.GetComponent<Collider>();
-//        if (prb != null) prb.isKinematic = true;
-//        if (pcol != null) pcol.enabled = false;
-
-//        // Parent al seat
-//        //player.SetParent(playerSeat);
-//        //player.localPosition = Vector3.zero;
-//        //player.localRotation = Quaternion.identity;
-
-//        player.position = playerSeat.position;
-//        player.rotation = playerSeat.rotation;
-
-//        isPlayerInside = true;
-
-//        if (terminalAnimator != null) terminalAnimator.ShowTerminal();
-//        else if (cartTerminalUI != null) cartTerminalUI.gameObject.SetActive(true);
-
-//        Cursor.visible = true;
-//        Cursor.lockState = CursorLockMode.None;
-//    }
-
-//    public void ExitCart()
-//    {
-//        if (!isPlayerInside) return;
-
-//        isPlayerInside = false;
-
-//        if (playerController != null) playerController.enabled = true;
-
-//        Rigidbody prb = player.GetComponent<Rigidbody>();
-//        Collider pcol = player.GetComponent<Collider>();
-//        if (prb != null) prb.isKinematic = false;
-//        if (pcol != null) pcol.enabled = true;
-
-//        player.SetParent(null);
-//        player.position += transform.right * 1f + Vector3.up * 0.5f;
-
-//        if (terminalAnimator != null) terminalAnimator.HideTerminal();
-//        else if (cartTerminalUI != null) cartTerminalUI.gameObject.SetActive(false);
-
-//        Cursor.visible = false;
-//        Cursor.lockState = CursorLockMode.Locked;
-
-//        player = null;
-//        playerController = null;
-//    }
-//    #endregion
-//}
-
-using UnityEngine;
+﻿using UnityEngine;
 
 [System.Serializable]
 public class CartSpeedSettings
@@ -247,5 +123,33 @@ public class MinecartController : MonoBehaviour
         player = null;
         playerController = null;
     }
+
+    private void OnDestroy()
+    {
+        // Si la vagoneta se destruye con el player dentro
+        if (isPlayerInside && player != null)
+        {
+            Debug.Log("[MinecartController] Vagoneta destruida con el player dentro. Liberando player.");
+
+            // Reactivar control del player
+            if (playerController != null)
+                playerController.enabled = true;
+
+            Rigidbody prb = player.GetComponent<Rigidbody>();
+            Collider pcol = player.GetComponent<Collider>();
+
+            if (prb != null) prb.isKinematic = false;
+            if (pcol != null) pcol.enabled = true;
+
+            player.SetParent(null);
+
+            // Pequeño offset de seguridad
+            player.position += Vector3.up * 1f;
+
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
     #endregion
 }
