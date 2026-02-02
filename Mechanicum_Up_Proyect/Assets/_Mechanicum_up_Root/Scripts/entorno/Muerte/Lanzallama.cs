b74f09946397c5aef25680fec566a3e65d60c8cf
Sadh0w <alexgalindosandin@gmail.com>
2026-02-02 13:42:40 +0100

    intento sonido fuego

--- Mechanicum_Up_Proyect/Assets/_Mechanicum_up_Root/Scripts/entorno/Muerte/Lanzallama.cs
+++ Mechanicum_Up_Proyect/Assets/_Mechanicum_up_Root/Scripts/entorno/Muerte/Lanzallama.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class ChimeneaLanzallamas : MonoBehaviour
 {
+    [Header("Lanzallamas")]
     public GameObject lanzallamasPrefab; // El prefab "Lanzallamas"
     public Transform spawnPoint;         // Punto de salida del fuego
     public float fireInterval = 2f;      // Cada cuánto tiempo dispara
+
+    [Header("Audio")]
+    public AudioSource audioSource;      // Fuente de audio
+    public AudioClip fireSFX;            // Sonido de disparo del lanzallamas
 
     private float timer;
 
@@ -21,7 +26,13 @@ public class ChimeneaLanzallamas : MonoBehaviour
 
     void Disparar()
     {
-        // Instancia el prefab en el spawnPoint, sin moverse
+        // Instancia el prefab en el spawnPoint
         Instantiate(lanzallamasPrefab, spawnPoint.position, spawnPoint.rotation);
+
+        // Reproduce el SFX
+        if (audioSource != null && fireSFX != null)
+        {
+            audioSource.PlayOneShot(fireSFX);
+        }
     }
 }
